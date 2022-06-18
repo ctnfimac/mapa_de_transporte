34f3d229be39119ee06ccd45a33c9053b5eb8bdd
Christian Peralta <christianipc.1987@gmail.com>
2022-06-18 14:41:12 -0300

    Agrego el mapa base y la posibilidad de ver los usuarios en el mismo con un marker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
+import useSwr from "swr";
+
+const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 function App() {
+  const url = 'http://localhost:3001/usuarios'
+  // const url = 'https://epok.buenosaires.gob.ar/getGeoLayer/?categoria=programas_de_salud_en_otras_instituciones&formato=geojson&srid=4326&especialidad=mac'
+  const {data, error} = useSwr(url, fetcher)
+  
+  const usuarios = data && !error ? data.slice(0,100): [];
+  // const usuarios = data && !error ? data.features.slice(0,100): [];
+  // console.log(usuarios)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[-34.62, -58.44]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://servicios.usig.buenosaires.gob.ar/mapcache/tms/1.0.0/amba_con_transporte_3857@GoogleMapsCompatible/{z}/{x}/{-y}.png"
+      />
+      {usuarios && usuarios.map(usuario => 
+          // <Marker 
+          //   key = {usuario.id}
+          // position={[usuario.geometry.coordinates[1], usuario.geometry.coordinates[0]]}
+          // />
+          <Marker 
+            key = {usuario.id}
+            position={[usuario.latitud,usuario.longitud]}
+          />
+        )
+      }
+    </MapContainer>
   );
 }
 
