2aa43a3d86b9afc17cb331571f746afe45f5f394
Christian Peralta <christianipc.1987@gmail.com>
2022-06-18 17:31:17 -0300

    Agrego tooltips a los puntos de los usuarios y reemplazo el marker por un circle

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Popup, Circle } from 'react-leaflet';
 import './App.css';
 import useSwr from "swr";
 
 const fetcher = (...args) => fetch(...args).then(response => response.json());
+const greenOptions = { color: '#5f27cd', fillColor: '#5f27cd', fillOpacity: 1}
+
 
 function App() {
-  const url = 'http://localhost:3001/usuarios'
-  // const url = 'https://epok.buenosaires.gob.ar/getGeoLayer/?categoria=programas_de_salud_en_otras_instituciones&formato=geojson&srid=4326&especialidad=mac'
-  const {data, error} = useSwr(url, fetcher)
-  
+  const url = 'http://localhost:3001/usuarios';
+
+  const {data, error} = useSwr(url, fetcher);
   const usuarios = data && !error ? data.slice(0,100): [];
-  // const usuarios = data && !error ? data.features.slice(0,100): [];
-  // console.log(usuarios)
+
   return (
-    <MapContainer center={[-34.62, -58.44]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[-34.62, -58.44]} zoom={13} maxZoom={14} minZoom={12} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://servicios.usig.buenosaires.gob.ar/mapcache/tms/1.0.0/amba_con_transporte_3857@GoogleMapsCompatible/{z}/{x}/{-y}.png"
       />
       {usuarios && usuarios.map(usuario => 
-          // <Marker 
-          //   key = {usuario.id}
-          // position={[usuario.geometry.coordinates[1], usuario.geometry.coordinates[0]]}
-          // />
-          <Marker 
-            key = {usuario.id}
-            position={[usuario.latitud,usuario.longitud]}
-          />
+          <Circle
+          center={[usuario.latitud, usuario.longitud]}
+          pathOptions={greenOptions}
+          radius={50}
+        >
+          <Popup>
+            <b>{usuario.nombre}</b> <br/>
+            latitud: {usuario.latitud}<br/>
+            longitud: {usuario.longitud}
+          </Popup>
+          </Circle>
         )
       }
     </MapContainer>
