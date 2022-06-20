a735f30a293fcd6179a0b814c4078cfd5037fc9a
Christian Peralta <christianipc.1987@gmail.com>
2022-06-20 16:54:01 -0300

    Admin, agrego los pasos para borrar un usuario

--- src/components/Admin/listado/Usuarios.js
+++ src/components/Admin/listado/Usuarios.js
@@ -4,12 +4,25 @@ import './Usuarios.css';
 
 const Usuarios = () => {
     const [usuarios, setUsuarios] = useState([])
+    const [datosActualizados, setDatosActualizados] = useState(false)
 
     useEffect(()=>{
         fetch('http://localhost:3001/usuarios')
             .then(response => response.json())
             .then(data => setUsuarios(data))
-    },[])
+    },[datosActualizados])
+
+    const borrar = (id) =>{
+        let confirmacion = window.confirm(`Realmente quiere borrar el registro de id: ${id} ?`)
+        if(confirmacion){
+            console.log('borrar')
+            fetch('http://localhost:3001/usuario/'+id, {
+                method:'DELETE'
+            })
+            .then(response => response.text())
+            .then(data => setDatosActualizados(!datosActualizados))
+        }
+    }
 
     return (
         <div className="usuarios__container">
@@ -35,7 +48,10 @@ const Usuarios = () => {
                             <td>{item.longitud}</td>
                             <td>{item.latitud}</td>
                             <td>
-                                <button className="btn-danger">
+                                <button 
+                                    className="btn-danger"
+                                    onClick={() => borrar(item.id)}
+                                >
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="10" fill="currentColor" viewBox="0 0 16 16">
                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                     </svg>
