41ee6d6d891440e7b96474c6839878346906414a
Christian Peralta <christianipc.1987@gmail.com>
2022-12-26 15:22:30 -0300

    Testing/Usuario test de renderizado de valores

--- src/__test__/components/Admin/listado/Usuario.test.js
+++ src/__test__/components/Admin/listado/Usuario.test.js
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom'
-import { mount, shallow } from "enzyme"
+import { shallow } from "enzyme"
 import Usuario from "../../../../components/Admin/listado/Usuario"
-import { BrowserRouter } from "react-router-dom";
 
 
 describe('<Usuario />', () =>{
@@ -11,13 +10,13 @@ describe('<Usuario />', () =>{
     beforeAll(() => {
         borrar = jest.fn()
         item = {
-            id: 1,
+            id: '2',
             nombre: 'Christian',
             direccion: 'Las tunas 11122',
             barrio: 'liniers',
-            comuna: 9,
-            longitud: -54.2,
-            latitud: -34.6
+            comuna: '9',
+            longitud: '-54.2',
+            latitud: '-34.6'
         }
         usuario = shallow(<Usuario item={item} borrar={borrar} />)
 
@@ -41,4 +40,21 @@ describe('<Usuario />', () =>{
         expect(usuario.find('.btn-danger').text()).toEqual('eliminar')
         expect(usuario.find('.btn-warning').text()).toEqual('modificar')
     })
+    
+    test('Los valores de cada columna se renderizan correctamente', () => {
+        let columnas = usuario.find('.usuario_info > td')
+        // compruebo que hay 8 columnas
+        expect(columnas.length).toEqual(8)
+        
+        const valores = usuario.find('.usuario_info').first().find('td').map(column => column.text())
+        expect(valores[0]).toEqual(item['id'])
+        expect(valores[1]).toEqual(item['nombre'])
+        expect(valores[2]).toEqual(item['direccion'])
+        expect(valores[3]).toEqual(item['barrio'])
+        expect(valores[4]).toEqual(item['comuna'])
+        expect(valores[5]).toEqual(item['longitud'])
+        expect(valores[6]).toEqual(item['latitud'])
+
+    })
+
 })
